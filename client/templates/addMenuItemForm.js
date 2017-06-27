45e51c2974e8e43fc66a3be2edb9e187c169936a
bokwoon <bokwoon.c@gmail.com>
2017-06-27 15:45:07 +0800

    added some sembalnce of an order system to menupage

---
+++ client/templates/addMenuItemForm.js
@@ -0,0 +1,19 @@
+Template.addMenuItemForm.events({
+	'submit form': function(event) {
+		event.preventDefault(); //prevents page from auto-refreshing when form is submitted
+		const menuItemName = event.target.menuItemName.value; //pull the value (i.e. the string) from the html item with name="menuItemName" and assign it to the const menuItemName
+		MenuList.insert({
+			menuitem: menuItemName
+		});
+		event.target.menuItemName.value=""; //clear the form after submission
+	}
+});
+
+Template.addMenuItemForm.events({
+	'click .remove2': function() {
+		const selectedMenuItem = Session.get('sMII');
+		MenuList.remove({
+			_id: selectedMenuItem
+		});
+	},
+});
--- client/templates/menuPage.js
+++ client/templates/menuPage.js
@@ -1,16 +1,59 @@
 Template.menuPage.helpers({
-	menulist: function() {
+	'menulist' : function() {
 		return MenuList.find();
 	},
-	standingorders: function() {
+
+	'selectedMenuItem': function() {
+		const menuItemId = this._id;
+		const selectedMenuItemId = Session.get('sMII');
+		if (menuItemId == selectedMenuItemId) {
+			return "selected";
+		}
+	},
+
+	'selectedOrderItem': function() {
+		const orderItemId = this._id;
+		const selectedOrderItemId = Session.get('sOII');
+		if (orderItemId == selectedOrderItemId) {
+			return "selected";
+		}
+	},
+
+	'standingorders' : function() {
 		return StandingOrders.find({});
-	}
+	},
+
 });
 
 Template.menuPage.events({
-	'click .menuitm': function(){
+	'click .menuitm': function() {
+		const menuItemId = this._id;
+		Session.set('sMII', menuItemId);
 		StandingOrders.insert({
 			menuitem: this.menuitem
 		});
-	}
+	},
+
+	'click .orderitm': function() {
+		const orderItemId = this._id;
+		Session.set('sOII', orderItemId);
+	},
+
+	'click .remove': function() {
+		const selectedMenuItem = Session.get('sMII');
+		MenuList.remove({
+			_id: selectedMenuItem
+		});
+	},
+
+	'click .removeOrder': function() {
+		const selectedOrderItem = Session.get('sOII');
+		StandingOrders.remove({
+			_id: selectedOrderItem
+		});
+	},
+
+	'click .sendOrders': function() {
+		//
+	},
 });
