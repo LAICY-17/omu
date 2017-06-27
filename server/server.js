a4599f388a3dcff5955ed3a0c3916748c0de563b
bokwoon <bokwoon.c@gmail.com>
2017-06-27 18:24:31 +0800

    added .remove() functions to server code for client-side access

---
+++ server/server.js
@@ -0,0 +1,11 @@
+Meteor.startup(function() {
+	return Meteor.methods({
+		clearStandingOrders: function() {
+			return StandingOrders.remove({});
+		},
+
+		clearConfirmedOrders: function() {
+			return ConfirmedOrders.remove({});
+		},
+	});
+});
