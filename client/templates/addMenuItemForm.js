28d58fea655b4082e6710f3042014dd35df03279
bokwoon <bokwoon.c@gmail.com>
2017-06-27 19:02:46 +0800

    //comments

--- client/templates/addMenuItemForm.js
+++ client/templates/addMenuItemForm.js
@@ -1,7 +1,8 @@
 Template.addMenuItemForm.events({
 	'submit form': function(event) {
 		event.preventDefault(); //prevents page from auto-refreshing when form is submitted
-		const menuItemName = event.target.menuItemName.value; //pull the value (i.e. the string) from the html item with name="menuItemName" and assign it to the const menuItemName
+		const menuItemName = event.target.menuItemName.value;
+		//^ pull the value (i.e. the string) from the html item with name="menuItemName" and assign it to the const menuItemName
 		MenuList.insert({
 			menuitem: menuItemName
 		});
