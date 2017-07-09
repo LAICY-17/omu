8400b826670269229b63f6a087b4690be4a7c349
bokwoon <bokwoon.c@gmail.com>
2017-07-09 17:22:36 +0800

    changed tableLogin format

--- client/templates/tableLogin.js
+++ client/templates/tableLogin.js
@@ -1,12 +1,18 @@
+Template.tableLogin.helpers({
+	'currentMeteorUser' : function() {
+		return Meteor.userId();
+	},
+});
+
 Template.tableLogin.events({
 	'submit form': function(event) {
 		event.preventDefault();
 		const tableLoginID = event.target.tableLoginID.value;
-		const tableCheckNo = tableLoginID % 10000;
-		const tableNo = parseInt(tableLoginID / 10000);
-		console.log(tableCheckNo);
-		console.log(tableNo);
-		if (tableCheckNo!=1234) {
+		const parsedTLID = tableLoginID.split('-');
+		const rcode = parsedTLID[0];
+		const tablenum = parsedTLID[1];
+		const vcode = parsedTLID[2];
+		if (vcode!=1234) {
 			document.getElementById("loginErrorMsg").innerHTML = "Incorrect Table ID, please check your table to see if you have keyed in the correct Table ID."
 		} else {
 			Router.go('/menu');
