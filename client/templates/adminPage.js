5f51a66cec23223c9a7b7cfd8953e794ac091229
bokwoon <bokwoon.c@gmail.com>
2017-07-10 21:33:57 +0800

    added more adminPage functions

--- client/templates/adminPage.js
+++ client/templates/adminPage.js
@@ -29,13 +29,74 @@ Template.adminPage.events({
 		event.preventDefault();
 		const Rcode = event.target.Rcode.value;
 		event.target.Rcode.value="";
-		console.log(Rcode);
-		const x = Meteor.userId();
-		Meteor.call('updateRcode', [x, Rcode]);
+		console.log("new rcode is: " + Rcode);
+
+		// v This Method only updates the first occurence, not ever occurence like it should (bug?)
+		// Meteor.call('updateRcode', Meteor.userId(), Rcode);
+
+		// v This method cannot be executed as it edits multiple documents
+		// v and client side code is untrusted to do that
 		// OmuIRTV.update(
 		// 	{ meteorUserId: Meteor.userId() },
 		// 	{ $set: { rcode: Rcode } },
 		// );
+
+		const doesRcodeExist = OmuIRTV.find(
+			{ meteorUserId: Meteor.userId(),
+				rcode: { $exists: true } }
+		).count();
+		if (doesRcodeExist > 0) {
+			currRcode = OmuIRTV.findOne({
+				meteorUserId: Meteor.userId()
+			}).rcode;
+			// v Post Condition: rcode fields for this user are not empty
+			do {
+				var currRcodeCount = OmuIRTV.find({
+					meteorUserId: Meteor.userId(),
+					rcode: currRcode,
+				}).count();
+				console.log("a: " + currRcodeCount);
+				if (currRcodeCount > 0) {
+					var currRcodeId = OmuIRTV.findOne({
+						meteorUserId: Meteor.userId(),
+						rcode: currRcode,
+					})._id;
+					OmuIRTV.update(
+						{ _id: currRcodeId },
+						{ $set: { rcode: Rcode }}
+					);
+				}
+				var currRcodeCount = OmuIRTV.find({
+					meteorUserId: Meteor.userId(),
+					rcode: currRcode,
+				}).count();
+				console.log("b: " + currRcodeCount);
+			} while (currRcodeCount > 0);
+		} else {
+			// v Post Condition: Either one or many documents have no rcode field
+			do {
+				var currEmptyRcodeCount = OmuIRTV.find({
+					meteorUserId: Meteor.userId(),
+					rcode: { exists: false },
+				}).count();
+				console.log("c: " + currRcodeCount);
+				if (currEmptyRcodeCount > 0) {
+					var currRcodeId = OmuIRTV.findOne({
+						meteorUserId: Meteor.userId(),
+						rcode: { exists: false },
+					})._id;
+					OmuIRTV.update(
+						{ _id: currRcodeId },
+						{ $set: { rcode: Rcode }}
+					);
+				}
+				var currEmptyRcodeCount = OmuIRTV.find({
+					meteorUserId: Meteor.userId(),
+					rcode: { exists: false },
+				}).count();
+				console.log("d: " + currRcodeCount);
+			} while (currEmptyRcodeCount > 0);
+		}
 	},
 
 	'click .addTable': function() {
@@ -77,6 +138,14 @@ Template.adminPage.events({
 			meteorUserId: Meteor.userId()
 		}).count();
 		if (SOC == 0 && COC == 0 && TC > 1) {
+			const table_id = OmuIRTV.findOne({
+				meteorUserId: Meteor.userId(),
+				tablenum: TC,
+			})._id;
+			console.log(table_id);
+			OmuIRTV.remove({ _id: table_id });
+		} else {
+			console.log("Either Standing Orders or Confirmed Orders is not empty, make sure both are empty before rmeoving tables");
 		}
 	},
 
