a29e4ebbe0ed2be9ae5aa6873d9409ab863473ba
bokwoon <bokwoon.c@gmail.com>
2017-07-10 16:55:28 +0800

    published every collection

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,15 @@
-// Meteor.publish('menulist', function() {
-// 	return MenuList.find({});
-// });
-// Meteor.publish('standingorders', function() {
-// 	return StandingOrders.find({});
-// });
+Meteor.publish('menulist', function() {
+	return MenuList.find({});
+});
+
+Meteor.publish('standingorders', function() {
+	return StandingOrders.find({});
+});
+
+Meteor.publish('confirmedorders', function() {
+	return ConfirmedOrders.find({});
+});
+
+Meteor.publish('omuirtv', function() {
+	return OmuIRTV.find({});
+});
