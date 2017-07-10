88b60bdc49d96b999a8fac11f2d10c5a27a93bfd
bokwoon <bokwoon.c@gmail.com>
2017-07-11 00:26:46 +0800

    now every menu item added will have the correct restaurant id tagged to it

--- client/templates/addMenuItemForm.js
+++ client/templates/addMenuItemForm.js
@@ -4,7 +4,8 @@ Template.addMenuItemForm.events({
 		const menuItemName = event.target.menuItemName.value;
 		//^ pull the value (i.e. the string) from the html item with name="menuItemName" and assign it to the const menuItemName
 		MenuList.insert({
-			menuitem: menuItemName
+			menuitem: menuItemName,
+			restAcc: Meteor.userId(),
 		});
 		event.target.menuItemName.value=""; //clear the form after submission
 	}
